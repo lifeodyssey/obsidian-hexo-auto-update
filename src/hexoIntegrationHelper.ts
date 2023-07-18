d7d3acd68a6e07fa3ef776f8203f70b2aef1798b
lifeodyssey <zhouthepassion@outlook.com>
2023-07-18 18:30:54 +0800

    feat: use different folder for mac and windows, add validate and load on start behave

--- src/hexoIntegrationHelper.ts
+++ src/hexoIntegrationHelper.ts
@@ -1,36 +1,67 @@
 import {App, FileSystemAdapter} from 'obsidian';
 import {SimpleGit, StatusResult} from 'simple-git';
 import * as os from 'os';
+import * as fs from 'fs';
+
 
 const symlinkDir = require('symlink-dir');
 const path = require('path');
 
 
-export async function createHexoSymlink(app: App, hexoSourcePath: string, targetFolder: string): Promise<string> {
+export async function createSystemSpecificSymlink(app: App, hexoSourcePath: string): Promise<string> {
 	try {
 		const vaultPath = (app.vault.adapter as FileSystemAdapter).getBasePath();
-		const hexoFolderName = hexoSourcePath.split('/').pop();
-		const newFolderPath = path.join(vaultPath, targetFolder, hexoFolderName); // <--- changed this
-
 		const isWindows = os.platform() === 'win32';
-		var result;
-		if (isWindows) {
-			// Create a directory junction on Windows
-			result = await symlinkDir(hexoSourcePath, newFolderPath, 'junction');
+		const targetFolder = isWindows ? 'Win Blog' : 'Mac Blog';
+		const newFolderPath = path.join(vaultPath, targetFolder);
+
+		if (!fs.existsSync(newFolderPath) || !fs.lstatSync(newFolderPath).isSymbolicLink()) {
+			var result;
+			if (isWindows) {
+				result = await symlinkDir(hexoSourcePath, newFolderPath, 'junction');
+			} else {
+				result = await symlinkDir(hexoSourcePath, newFolderPath);
+			}
+
+			console.log('Symlink successfully created:', newFolderPath);
 		} else {
-			// Create a symlink on other systems (e.g., macOS, Linux)
-			result = await symlinkDir(hexoSourcePath, newFolderPath);
+			console.log('Symlink already exists and is valid:', newFolderPath);
 		}
 
-		if (result.reused) {
-			console.log('Symlink/junction already exists and has been reused:', newFolderPath);
-		} else {
-			console.log('Symlink/junction successfully created:', newFolderPath);
-		}
 		return 'success';
 	} catch (error) {
 		console.error('Failed to create symlink:', error);
 		return 'failure';
+	}
+}
+async function isSymlinked(hexoSourcePath: string, newFolderPath: string): Promise<boolean> {
+	try {
+		const stats = await fs.promises.lstat(newFolderPath);
+
+		if (stats.isSymbolicLink()) {
+			const symlinkTarget = await fs.promises.readlink(newFolderPath);
+
+			// Normalize the paths before comparing them, to account for possible differences in path formats
+			return path.normalize(hexoSourcePath) === path.normalize(symlinkTarget);
+		} else {
+			return false;
+		}
+	} catch (error) {
+		console.error('Failed to check if path is a symlink:', error);
+		return false;
+	}
+}
+export async function validateSymlink(app: App, hexoSourcePath: string): Promise<void> {
+	const vaultPath = (app.vault.adapter as FileSystemAdapter).getBasePath();
+	const isWindows = os.platform() === 'win32';
+	const targetFolder = isWindows ? 'Win Blog' : 'Mac Blog';
+	const newFolderPath = path.join(vaultPath, targetFolder);
+
+	if (!(await isSymlinked(hexoSourcePath, newFolderPath))) {
+		console.log('Symlink validation failed, attempting to create symlink...');
+		await createSystemSpecificSymlink(app, hexoSourcePath);
+	} else {
+		console.log('Symlink validation succeeded, no action needed.');
 	}
 }
 

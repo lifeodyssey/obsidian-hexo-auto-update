fab23c62383220ec316fac5682856139e93e2a90
lifeodyssey <zhouthepassion@outlook.com>
2023-11-22 11:30:04 +0800

    chore: add test config

--- jest.config.ts
+++ jest.config.ts
@@ -85,16 +85,16 @@ const config: Config = {
 	// ],
 
 	// An array of file extensions your modules use
-	// moduleFileExtensions: [
-	//   "js",
-	//   "mjs",
-	//   "cjs",
-	//   "jsx",
-	//   "ts",
-	//   "tsx",
-	//   "json",
-	//   "node"
-	// ],
+	moduleFileExtensions: [
+	  "js",
+	  "mjs",
+	  "cjs",
+	  "jsx",
+	  "ts",
+	  "tsx",
+	  "json",
+	  "node"
+	],
 
 	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
 	// moduleNameMapper: {},
@@ -153,7 +153,7 @@ const config: Config = {
 	// snapshotSerializers: [],
 
 	// The test environment that will be used for testing
-	// testEnvironment: "jest-environment-node",
+	testEnvironment: "jest-environment-node",
 
 	// Options that will be passed to the testEnvironment
 	// testEnvironmentOptions: {},
@@ -182,7 +182,9 @@ const config: Config = {
 	// testRunner: "jest-circus/runner",
 
 	// A map from regular expressions to paths to transformers
-	// transform: undefined,
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
 	// transformIgnorePatterns: [

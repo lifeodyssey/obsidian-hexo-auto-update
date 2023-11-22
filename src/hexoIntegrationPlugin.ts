1ec93b6fd4d0efc8efd85677735afeeee6990d72
lifeodyssey <zhouthepassion@outlook.com>
2023-11-22 15:28:58 +0800

    refactor: move the setting tabs into setting foldeer

--- src/hexoIntegrationPlugin.ts
+++ src/hexoIntegrationPlugin.ts
@@ -1,89 +1,90 @@
-import {Notice, Plugin,debounce, TFolder,TAbstractFile} from "obsidian";
+import {Notice, Plugin} from "obsidian";
 import {HexoIntegrationSettings} from "./types";
 import {DEFAULT_SETTINGS} from "./constants";
-import HexoIntegrationSettingsTab from "./hexoIntegrationSettingsTab";
-import {checkForChanges, commitChanges, createSystemSpecificSymlink, pushChanges,validateSymlink} from "./hexoIntegrationHelper";
+import HexoIntegrationSettingsTab from "./settings/hexoIntegrationSettingsTab";
 import {simpleGit, SimpleGit} from 'simple-git';
+import {checkForChanges, commitChanges, pushChanges} from "./git/gitOperations";
+import {createSystemSpecificSymlink, validateSymlink} from "./symlinks/symlinkOperation";
 
 export default class HexoIntegrationPlugin extends Plugin {
-	settings: HexoIntegrationSettings;
-	git: SimpleGit;
+    settings: HexoIntegrationSettings;
+    git: SimpleGit;
 
-	async onload() {
+    async onload() {
 
-		await this.loadSettings();
-		this.addSettingTab(new HexoIntegrationSettingsTab(this.app, this));
-		const hexoBlogPath = this.settings.hexoSourcePath;
+        await this.loadSettings();
+        this.addSettingTab(new HexoIntegrationSettingsTab(this.app, this));
+        const hexoBlogPath = this.settings.hexoSourcePath;
 
-		this.git = simpleGit(hexoBlogPath);
+        this.git = simpleGit(hexoBlogPath);
 
-		// Add the following line to validate the symlink when the plugin loads
-		await validateSymlink(this.app, hexoBlogPath);
+        // Add the following line to validate the symlink when the plugin loads
+        await validateSymlink(this.app, hexoBlogPath);
 
-		if (!this.settings.hexoSourcePath) {
-			new Notice('Please configure the path to your Hexo blog in the settings.');
-			return;
-		}
+        if (!this.settings.hexoSourcePath) {
+            new Notice('Please configure the path to your Hexo blog in the settings.');
+            return;
+        }
 
-		await this.createSymlink();
+        await this.createSymlink();
 
-		this.addSettingTab(new HexoIntegrationSettingsTab(this.app, this));
-		// Get the Hexo blog path from the plugin settings
-		// Initialize the SimpleGit instance with the Hexo blog path
-		this.git = simpleGit(hexoBlogPath);
-		// Call the `checkForChanges` function every minute (or any desired interval)
-		setInterval(async () => {
-			const status = await checkForChanges(this.git);
-			if (status != null) {
-				const changedFilesCount = status.created.length + status.modified.length + status.deleted.length;
+        this.addSettingTab(new HexoIntegrationSettingsTab(this.app, this));
+        // Get the Hexo blog path from the plugin settings
+        // Initialize the SimpleGit instance with the Hexo blog path
+        this.git = simpleGit(hexoBlogPath);
+        // Call the `checkForChanges` function every minute (or any desired interval)
+        setInterval(async () => {
+            const status = await checkForChanges(this.git);
+            if (status != null) {
+                const changedFilesCount = status.created.length + status.modified.length + status.deleted.length;
 
-				if (changedFilesCount > 0) {
-					console.log('Changed files:', status.files);
+                if (changedFilesCount > 0) {
+                    console.log('Changed files:', status.files);
 
-					// Commit and push the changes
-					try {
-						await commitChanges(this.git, status);
-						await pushChanges(this.git);
-						console.log('Changes committed and pushed successfully.');
-						new Notice('Changes committed and pushed successfully.');
+                    // Commit and push the changes
+                    try {
+                        await commitChanges(this.git, status);
+                        await pushChanges(this.git);
+                        console.log('Changes committed and pushed successfully.');
+                        new Notice('Changes committed and pushed successfully.');
 
-					} catch (error) {
-						console.error('Error during commit and push:', error);
-						throw new Error(`Error during commit and push:${error.message}`);
-					}
-				}
-			}
-		}, 60 * 1000);
-	}
+                    } catch (error) {
+                        console.error('Error during commit and push:', error);
+                        throw new Error(`Error during commit and push:${error.message}`);
+                    }
+                }
+            }
+        }, 60 * 1000);
+    }
 
-	onunload() {
-	}
+    onunload() {
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        );
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-	async createSymlink() {
-		try {
-			const status = await createSystemSpecificSymlink(this.app, this.settings.hexoSourcePath);
-			
+    async createSymlink() {
+        try {
+            const status = await createSystemSpecificSymlink(this.app, this.settings.hexoSourcePath);
 
-			// if (status === 'success') {
-			// 	// Refresh the Obsidian file explorer to show the newly created symlink
-			// 	this.refreshFiles();
-			// }
 
-			return status;
-		} catch (error) {
-			throw new Error(`Failed to create symlink: ${error.message}`);
-		}
-	}
+            // if (status === 'success') {
+            // 	// Refresh the Obsidian file explorer to show the newly created symlink
+            // 	this.refreshFiles();
+            // }
+
+            return status;
+        } catch (error) {
+            throw new Error(`Failed to create symlink: ${error.message}`);
+        }
+    }
 }
---
+++ src/settings/hexoIntegrationSettingsTab.ts
@@ -0,0 +1,72 @@
+import {App, Notice, PluginSettingTab, Setting} from "obsidian";
+import HexoIntegrationPlugin from "../hexoIntegrationPlugin";
+
+const path = require('path');
+const {dialog} = require('electron').remote;
+
+
+export default class HexoIntegrationSettingsTab extends PluginSettingTab {
+    plugin: HexoIntegrationPlugin;
+
+    constructor(app: App, plugin: HexoIntegrationPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const {containerEl} = this;
+
+        containerEl.empty();
+
+        containerEl.createEl("h2", {
+            text: "Settings for Hexo Integration Plugin.",
+        });
+
+        const hexoFolderSetting = new Setting(containerEl)
+            .setName("Hexo Source Path")
+            .setDesc("Specify the path to the Hexo source folder")
+            .addButton(button => {
+                button.setButtonText("Select Folder")
+                    .onClick(async () => {
+                        const result = await dialog.showOpenDialog({
+                            properties: ['openDirectory']
+                        });
+                        if (!result.canceled && result.filePaths.length > 0) {
+                            const selectedPath = result.filePaths[0];
+                            console.log("Selected Hexo Source Path: " + selectedPath);
+
+                            this.plugin.settings.hexoSourcePath = selectedPath;
+                            await this.plugin.saveSettings();
+                            new Notice("Hexo source path set to: " + selectedPath);
+                        }
+                    });
+            })
+        const addButton = hexoFolderSetting.addButton((button) =>
+            button
+                .setButtonText("Add")
+                .onClick(async () => {
+                    if (this.plugin.settings.hexoSourcePath) {
+                        addButton.setDisabled(true);
+
+                        const addingNotice = new Notice("Adding...");
+
+                        try {
+                            const status = await this.plugin.createSymlink();
+
+                            addingNotice.hide();
+
+                            new Notice(status);
+                        } catch (error) {
+                            new Notice(`Error: ${error.message}`);
+                        } finally {
+                            addButton.setDisabled(false);
+                        }
+                    } else {
+                        new Notice(
+                            "Please set the Hexo path in the plugin settings before creating the folder."
+                        );
+                    }
+                })
+        );
+    }
+}
